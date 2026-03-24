cd0d065ada24dbcf4f98268a03004ed6b4deb1c6
thyssem <thyssem.bazizi-makhlouf@epitech.eu>
2026-03-24 15:46:49 +0100

    refactor: remove unused utils folder
    - Remove old procedural functions replaced by OOP architecture
    - Cookie handling moved to ClientContext
    - Data loading moved to Repository pattern
    - Error handling integrated into controllers

--- utils/cookie.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Utils\Cookie;
-
-function getClient(): string {
-    $config = require __DIR__ . '/../config.php';
-    $client = $_COOKIE['client'] ?? $config['allowed_clients'][0];
-    return in_array($client, $config['allowed_clients']) ? $client : $config['allowed_clients'][0];
-}
--- utils/error.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Utils\Error;
-
-function renderError(string $message): void {
-    error_log('Tool4cars ' . $message);
-    echo '<p class="error">' . htmlspecialchars('Une erreur est survenue.') . '</p>';
-}
